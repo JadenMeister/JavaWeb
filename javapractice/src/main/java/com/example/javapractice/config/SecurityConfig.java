a7f3a5c36c01e6747ee62558f6b3639064b8c10c
JadenMeister <bigdragon00@naver.com>
2025-06-24 10:53:53 +0900

    Test: hello 띄우기용 (연습)

--- javapractice/src/main/java/com/example/javapractice/config/SecurityConfig.java
+++ javapractice/src/main/java/com/example/javapractice/config/SecurityConfig.java
@@ -10,18 +10,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers("/login", "/logout").permitAll() //엑세스
-                .anyRequest().authenticated() // 모든 요청은 인증이 필요
-                .and()
-            .formLogin()
-                .loginPage("/login") // 로그인 페이지로 이동
-                .permitAll() // 로그인 페이지는 모든 사용자에게 허용
-                .and()
-            .logout()
-                .logoutUrl("/logout") // 로그아웃 URL 설정
-                .permitAll(); // 로그아웃은 모든 사용자에게 허용
-
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()  // 모든 요청 허용 (테스트용)
+            );
+        
         return http.build();
     }
 }
